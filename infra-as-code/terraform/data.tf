c3040666ce3c28d9bf20d2ca40c6c412e831f2d8
Alessandro Vozza <alessandro.vozza@microsoft.com>
2025-08-07 15:13:20 +0200

    repeat

--- infra-as-code/terraform/data.tf
+++
@@ -1,6 +0,0 @@
-# Data sources
-data "azurerm_client_config" "current" {}
-
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
--- infra-as-code/terraform/readme.md
+++
@@ -1,5 +0,0 @@
-# OpenAI End-to-End Baseline Terraform infrastructure-as-code module
-
-## Prerequisites
-
-- An Azure Resource Group
